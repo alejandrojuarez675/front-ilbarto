694130e175580e9246c17d5a8ba3572228342d4d
Alejandro Juarez <alejandrojuarez675@gmail.com>
2021-07-13 16:44:20 -0300

    #7 add buttons in food cards

--- src/app/core/services/cart.service.ts
+++ src/app/core/services/cart.service.ts
@@ -72,6 +72,7 @@ export class CartService {
     const price = this.cart.products.map(x => x.price).reduce((a, b) => a + b, 0);
     this.cart.price = price;
 
+    console.log('cart', this.cart);
     this.subject.next(this.cart);
   }
 }
