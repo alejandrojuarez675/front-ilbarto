e8fc597f3e1d5cf2ead2227970e010c028f6233f
Alejandro Juarez <alejandrojuarez675@gmail.com>
2021-07-13 17:24:22 -0300

    #14 refactor to differentiate pages from component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './core/pages/not-found/not-found.component';
-import { ShopsComponent } from './modules/shops/shops/shops.component';
+import { ShopsComponent } from './modules/shops/pages/shops/shops.component';
 
 const routes: Routes = [
   { path: 'shops/:name', component: ShopsComponent },
--- src/app/modules/shops/shops.module.ts
+++ src/app/modules/shops/shops.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { ShopsComponent } from './shops/shops.component';
-import { FoodCardComponent } from './food-card/food-card.component';
-import { SectionsComponent } from './sections/sections.component';
+import { ShopsComponent } from './pages/shops/shops.component';
+import { FoodCardComponent } from './components/food-card/food-card.component';
+import { SectionsComponent } from './components/sections/sections.component';
 
 
 
