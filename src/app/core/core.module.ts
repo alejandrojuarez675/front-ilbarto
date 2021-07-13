a94ddaf0e2cb406580704d23b18a165f427d511d
Alejandro Juarez <alejandrojuarez675@gmail.com>
2021-07-13 17:00:23 -0300

    #10 move header to other module to add resume

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ShopService } from './http/shop.service';
 import { CartService } from './services/cart.service';
@@ -11,14 +10,12 @@ import { CartService } from './services/cart.service';
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderComponent,
     NotFoundComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent,
     FooterComponent,
   ],
   providers: [
