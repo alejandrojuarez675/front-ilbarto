2066d19b4659d5823169f94c5bf1c54997689f43
Alejandro Juarez <alejandrojuarez675@gmail.com>
2021-07-13 21:29:03 -0300

    #16 Add sending of wsp msg

--- src/app/core/utils/wsp-utils.ts
+++ src/app/core/utils/wsp-utils.ts
@@ -1,5 +1,7 @@
 import { Cart } from 'src/app/shared/models/cart.model';
 
+const INITIAL_MSG = 'Hola, me contacto desde ilBarto, quisiera encargarte lo siguiente: ';
+
 export function getUrlWsp(phone: any, cart: Cart): string {
   return (
     'https://api.whatsapp.com/send/?phone=' +
@@ -13,6 +15,6 @@ export function getUrlWsp(phone: any, cart: Cart): string {
 function formatCart(cart: Cart): string {
   return cart.products
     .map((p) => `${p.quantity} ${p.name}, `)
-    .reduce((a, b) => a + b, 'Hola, quisiera encargarte lo siguiente: ')
+    .reduce((a, b) => a + b, INITIAL_MSG)
     .concat(`.  El total presupuestado es ${cart.price}`);
 }
